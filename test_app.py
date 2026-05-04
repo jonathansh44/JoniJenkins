22de7b9a94f2b90f1787b887d8dfc0e3c117f67c
jonathansh44 <87603697+jonathansh44@users.noreply.github.com>
2026-05-04 21:21:08 +0300

    Add test for welcome message function

--- test_app.py
+++ test_app.py
@@ -1,2 +1,7 @@
+from models import get_welcome_message
+
+def test_welcome_message():
+    expected_message = "Joni is running this from GitHub Actions with MVC structure!"
+    assert get_welcome_message() == expected_message
 def test_always_passes():
     assert True
