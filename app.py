4b62dff0f6bdec6cdc57bb55d3ecf5adeadc5e32
jonathansh44 <87603697+jonathansh44@users.noreply.github.com>
2026-05-04 21:03:54 +0300

    Refactor welcome message in Flask app

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def get_welcome_message():
+    return "Joni is running this from GitHub Actions!"
+
 @app.route('/')
 def hello_world():
-    return '<h1>Hello DevOps World!</h1><p>Joni is running this from Jenkins and Docker.</p>'
+    
+    message = get_welcome_message()
+    return f'<h1>Hello DevOps World!</h1><p>{message}</p>'
 
 if __name__ == '__main__':
-    # חשוב: host='0.0.0.0' מאפשר גישה לאפליקציה מחוץ לקונטיינר
     app.run(debug=True, host='0.0.0.0', port=5000)
