3fd1aed18963b12c3c431615edd1d70dcfb2ff51
jonathansh44 <87603697+jonathansh44@users.noreply.github.com>
2026-05-04 21:23:24 +0300

    Add home route test to test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,14 @@
 from models import get_welcome_message
+from app import app
 
 def test_welcome_message():
     expected_message = "Joni is running this from GitHub Actions with MVC structure!"
     assert get_welcome_message() == expected_message
 def test_always_passes():
     assert True
+
+def test_home_route():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello DevOps World!' in response.data
